625ed3a88e720c43d50ccdd94c4893c1d1926ab4
DeepViolet <liuhuiyi1989@gmail.com>
2017-05-25 16:49:37 +0800

    SimpleChannelInboundHandler 的 泛型需要与服务器的传输信息类型一致，ByteBuf显然不一致，改为一致后信息传输解决

--- src/client/EchoClientHandler.java
+++ src/client/EchoClientHandler.java
@@ -1,26 +1,32 @@
 package client;
 
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
 
 /**
  * Created by liuhuiyi on 2017/5/25.
  */
-public class EchoClientHandler extends ChannelInboundHandlerAdapter {
+class EchoClientHandler extends SimpleChannelInboundHandler<String> {
 
-
-    @Override
+    /**
+     *此方法会在连接到服务器后被调用
+     * */
     public void channelActive(ChannelHandlerContext ctx) {
-        System.out.println("Hello Netty Server");
+        System.out.println("Netty rocks!");
+//        ctx.write(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
     }
 
+    /**
+     * 接收到服务器数据时调用
+     */
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        System.out.println(ctx.channel().remoteAddress()+"-->Netty Client read Message:"+msg);
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println("Client received: " + msg);
     }
 
-
-    @Override
+    /**
+     *捕捉到异常
+     * */
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         cause.printStackTrace();
         ctx.close();
